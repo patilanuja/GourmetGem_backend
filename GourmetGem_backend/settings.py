e8b7709ae1054611240d4a396de7b950179f109e
Anuja Patil <patilanuja98@gmail.com>
2024-08-06 05:41:33 -0700

    updated settings

--- GourmetGem_backend/settings.py
+++ GourmetGem_backend/settings.py
@@ -10,7 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from decouple import config
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY') if "SECRET_KEY" in os.environ["SECRET_KEY"] else config("SECRET_KEY")
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
