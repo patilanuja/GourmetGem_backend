225069fbd31e18b3afc24b635e30e3f77330d6f9
Anuja Patil <42777296+patilanuja@users.noreply.github.com>
2024-08-12 22:59:26 -0700

    Update settings.py
    fixed os error

--- GourmetGem_backend/settings.py
+++ GourmetGem_backend/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-# import os
+import os
 from pathlib import Path
 # from decouple import config
 # from dotenv import load_dotenv
